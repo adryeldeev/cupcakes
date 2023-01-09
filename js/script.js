42d7bc48f9084ee37249d0015cc863618eec08a3
Adryel martins <adryeldev@hotmail.com>
2023-01-09 16:01:22 -0300

    alteracao

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-var swiper=new Swiper(".slide-content",{slidesPerView:3,spaceBetween:25,loop:!0,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0,dynamincBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{0:{slidesPerView:1},520:{slidesPerView:2},950:{slidesPerView:3}}});
+var swiper = new Swiper(".slide-content", { slidesPerView: 3, spaceBetween: 15, loop: !0, centerSlide: "true", fade: "true", grabCursor: "true", pagination: { el: ".swiper-pagination", clickable: !0, dynamincBullets: !0 }, navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+ breakpoints: { 0: { slidesPerView: 1 }, 408: { slidesPerView: 1 }, 
+ 520: { slidesPerView: 0 }, 950: { slidesPerView: 3 },
+  768: { slidesPerView: 3 } } });

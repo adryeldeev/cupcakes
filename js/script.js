4558ef31add17ad3cabd2d1427f48fa1607cc07d
Adryel martins <adryeldev@hotmail.com>
2023-01-13 23:21:00 -0300

    atualizacao 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,1 @@
-
-var swiper = new Swiper(".slide-content", { slidesPerView: 3, spaceBetween: 15, loop: !0, centerSlide: "true", fade: "true", grabCursor: "true", pagination: { el: ".swiper-pagination", clickable: !0, dynamincBullets: !0 }, navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
- breakpoints: { 0: { slidesPerView: 1 }, 408: { slidesPerView: 1 }, 
- 520: { slidesPerView: 0 }, 950: { slidesPerView: 3 },
-  768: { slidesPerView: 3 } } });
+var swiper=new Swiper(".slide-content",{slidesPerView:3,spaceBetween:15,loop:!0,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0,dynamincBullets:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{0:{slidesPerView:1},408:{slidesPerView:1},520:{slidesPerView:0},950:{slidesPerView:3},768:{slidesPerView:3}}});const btnMobile=document.getElementById("btn-mobile");function toggleMenu(a){"touchstart"===a.type&&a.preventDefault();const b=document.getElementById("nav");b.classList.toggle("active");const c=b.classList.contains("active");a.currentTarget.setAttribute("aria-expanded",c),c?a.currentTarget.setAttribute("aria-label","Fechar Menu"):a.currentTarget.setAttribute("aria-label","Abrir Menu")}btnMobile.addEventListener("click",toggleMenu),btnMobile.addEventListener("touchstart",toggleMenu);
